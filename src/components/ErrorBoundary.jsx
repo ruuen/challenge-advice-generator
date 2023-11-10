b9d50a1074c3bc4403aaaf7198d6db73e1b43c17
Max Ruuen <max@madebyruuen.com>
2023-11-11 10:33:48 +1100

    Add ErrorBoundary logic, add ErrorFallback component

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,1 +1,26 @@
-// TODO: Error boundary component implementation here
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  //   componentDidCatch(error, info) {
+  //     //
+  //   }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
---
+++ src/components/ErrorFallback.jsx
@@ -0,0 +1,18 @@
+import "./ErrorFallback.scss";
+
+function ErrorFallback() {
+  return (
+    <div className="app-error">
+      <h2 className="app-error__heading">An error occurred</h2>
+      <p className="app-error__content">
+        There was a problem with the Advice Generator app, and it wasn't your
+        fault.
+      </p>
+      <p className="app-error__content">
+        Please refresh the page and try again.
+      </p>
+    </div>
+  );
+}
+
+export default ErrorFallback;
